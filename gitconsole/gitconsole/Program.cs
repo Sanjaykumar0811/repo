370837731e5c0254db8ffe7dfa7e57234c1f940f
SanjayKumar0811 <Sanjay9840881446@gmail.com>
2022-05-30 18:44:26 +0530

    ADD Console

---
+++ gitconsole/gitconsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gitconsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
