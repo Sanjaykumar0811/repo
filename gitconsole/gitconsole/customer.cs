8809080e8c71fa1545789e2632b27d5bb4bf2fb3
SanjayKumar0811 <Sanjay9840881446@gmail.com>
2022-05-30 21:03:32 +0530

    added customer

---
+++ gitconsole/gitconsole/customer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace gitconsole
+{
+    class customer
+    {
+        public int Code { get; set; }
+        public string Cust_Name { get; set; }
+    }
+}
