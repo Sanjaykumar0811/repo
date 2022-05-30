0208b68ef764cfec9810ad541fbf66f8ad3af86d
SanjayKumar0811 <Sanjay9840881446@gmail.com>
2022-05-30 18:46:43 +0530

    modified Console

--- gitconsole/gitconsole/Program.cs
+++ gitconsole/gitconsole/Program.cs
@@ -6,7 +6,7 @@ namespace gitconsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello sandy!?");
         }
     }
 }
